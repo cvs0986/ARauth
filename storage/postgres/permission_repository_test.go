446042fc6ec167d650eca66c6bfaa138c399b6c0
Veer Singh <veer.singh@nuagebiz.tech>
2026-01-07 17:29:01 +0530

    fix: Remove unused testutil import from permission_repository_test.go
    Fixes final CI compilation error
    
    Related to #10

--- storage/postgres/permission_repository_test.go
+++ storage/postgres/permission_repository_test.go
@@ -9,7 +9,6 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/google/uuid"
 	"github.com/nuage-identity/iam/identity/models"
-	"github.com/nuage-identity/iam/internal/testutil"
 	"github.com/nuage-identity/iam/storage/interfaces"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
