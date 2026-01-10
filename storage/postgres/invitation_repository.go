65a69832ca241c55cd8a006c34877e40dee0dd0d
Veer Singh <veer.singh@nuagebiz.tech>
2026-01-10 16:30:01 +0530

    fix(invitations): Fix final pq.Array type error

--- storage/postgres/invitation_repository.go
+++ storage/postgres/invitation_repository.go
@@ -43,7 +43,7 @@ func (r *InvitationRepository) Create(ctx context.Context, invitation *models.Us
 	}
 
 	// Convert role IDs to UUID array
-	var roleIDsArray pq.Array
+	var roleIDsArray interface{}
 	if len(invitation.RoleIDs) > 0 {
 		roleIDsArray = pq.Array(invitation.RoleIDs)
 	} else {
